41c18a6809159241c2eba3f4bb4ff8b0bb847c0e
khosasarang-code <khosasarang@gmail.com>
2026-05-28 18:50:58 -0400

    Refactor NewsTicker positioning in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,16 +38,16 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="min-h-screen bg-white antialiased">
+        <div style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 40,
+        }}>
+          <NewsTicker />
+        </div>
         <div style={{ paddingTop: "36px" }}>
-          <div style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            zIndex: 50,
-          }}>
-            <NewsTicker />
-          </div>
           {children}
         </div>
       </body>
